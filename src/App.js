dc60bc746e226c403276f1e490fe95ad3a15aacd
vi5a7 <udamvisal18@kit.edu.kh>
2021-03-09 10:47:13 +0700

    made with react

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+        <div className="contain">
+          <Navbar/>
+          <div className="content">
+            <Switch>
+              <Route path='/' exact>
+                  <Home/>
+              </Route>
+              <Route path='/create'>
+                  <Create/>
+              </Route>
+              <Route path='/blogs/:id'>
+                  <BlogDetails/>
+              </Route>
+              <Route path='*'>
+                  <NotFound/>
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Create.js
@@ -0,0 +1,57 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = {title, body, author}
+
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false)
+            history.push('/')
+        })
+    } 
+
+    return ( 
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                />
+                <label>Blog Author:</label>
+                <select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Add Blog ... </button>}
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
